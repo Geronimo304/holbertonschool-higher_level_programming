2dd12d993f3236e376374e675541a331b1e0d5fe
geronimo304 <geronimonegreira06@gmail.com>
2025-05-15 18:01:39 +0000

    trabajo 1

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for item in my_list:
+        if not isinstance(item, int):
+            raise TypeError("All elements must be integers")
+        print("{:d}".format(item))
