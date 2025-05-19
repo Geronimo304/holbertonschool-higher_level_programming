f56be068b9ba451024c51319b4a5fdd5fc79abcc
root <root@GAMER.>
2025-05-19 20:01:38 -0300

    3aa

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,11 +5,14 @@ class square:
     """Representa un cuadrado"""
 
     def __init__(self, size=0):
-        """Inicializa una nueva instancia"""
+        """Inicializa una nueva instancia.
+        
+        Args:
+            size (int): El tamaño del cuadrado (por defecto 0)
+        """
 
         if not isinstance(size, int):
             raise TypeError("debe de ser un numero entero")
         if size < 0:
             raise ValueError("el tamaño debe de ser mayor a 0 ")
         self.__size = size
-        
