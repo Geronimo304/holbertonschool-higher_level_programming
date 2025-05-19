aee9c0c40b45e212a3ea7212c3e9b0fd2d82df0c
root <root@GAMER.>
2025-05-19 19:34:08 -0300

    3a

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Define una clase."""
+
+
 class Square:
+    """Representa un cuadrado."""
 
     def __init__(self, size):
-
+        """Inicializa una nueva instancia con tamaño."""
         self.__size = size
