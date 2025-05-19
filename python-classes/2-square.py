97c83e4f9e8f00f2a9e6c2765d19f3dfc672e44c
root <root@GAMER.>
2025-05-19 19:59:31 -0300

    2a

---
+++ python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defino la calse."""
+
+class square:
+    """Representa un cuadrado"""
+
+    def __init__(self, size=0):
+        """Inicializa una nueva instancia"""
+
+        if not isinstance(size, int):
+            raise TypeError("debe de ser un numero entero")
+        if size < 0:
+            raise ValueError("el tamaño debe de ser mayor a 0 ")
+        self.__size = size
+        
