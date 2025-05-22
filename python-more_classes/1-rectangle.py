2f7d41984dc271a43d824c5a2555a531c9d69259
root <root@GAMER.>
2025-05-22 19:32:33 -0300

    1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""define una clase rectangle."""
+
+class   Rectangle:
+    """representa un rectangulo"""
+
+    def __init__(self, ancho=0, alto=0):
+       
+       self.establecer_ancho(ancho)
+       self.establecer_alto(alto)
+
+    def obtener_ancho(self):
+        """decuelve el ancho"""
+        return self.__ancho
+    
+    def establecer_ancho(self, valor):
+        """establece el ancho del rectangulo, siempre en cuando sea entero"""
+        
+        if not isinstance(valor, int):
+            raise TypeError("NO ES UN ENTERO")
+        if valor < 0:
+            raise TypeError("DEBE SER MAYOR A 0")
+        self.__ancho = valor
+
+    def obtener_alto(self):
+        """Devuelve el alto"""
+        return self.__alto
+    
+    def establecer_alto(self, valor):
+        """establece el alto del rectangulo, siempre en cuando sea entero"""
+
+        if not isinstance(valor, int):
+            raise TypeError("NO ES ENTERO")
+        if valor < 0:
+            raise TypeError("ES MENOR A 0")
+        self.__alto = valor
+        
