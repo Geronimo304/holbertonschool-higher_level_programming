6617c474715417c2d482ef590a6ce56a7d038c0a
root <root@GAMER.>
2025-05-31 01:14:43 -0300

    trabajo 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""inicializo la funcion"""
+
+
+def is_same_class(obj, a_class):
+    """retorna verdadero o falso dependiendo de si es una funcion"""
+    return type(obj) is a_class
