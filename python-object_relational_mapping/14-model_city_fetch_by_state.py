cf49812d071aac5e2e9464adc238124d47daa7cf
geronimo304 <geronimonegreira06@gmail.com>
2025-07-06 23:00:59 -0300

    14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """
-Prints all City objects from the database hbtn_0e_14_usa.
-Format: <state name>: (<city id>) <city name>
-
-Usage:
-    ./14-model_city_fetch_by_state.py <mysql_username> <mysql_password> <database_name>
+Imprime todas las ciudades de la BD `hbtn_0e_14_usa`, mostrando
+<state name>: (<city id>) <city name>
+ordenadas por `cities.id`.
 """
 
 import sys
@@ -13,17 +11,30 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-if __name__ == "__main__":
-    username, password, db_name = sys.argv[1:4]
 
+if __name__ == "__main__":
+    # Argumentos: usuario, contraseña y nombre de la BD
+    if len(sys.argv) != 4:
+        sys.exit("Uso: ./14-model_city_fetch_by_state.py <user> <password> <db>")
+
+    user, pwd, db = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Crear engine y sesión
     engine = create_engine(
-        f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}",
+        f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db}",
         pool_pre_ping=True,
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(City).join(State).order_by(City.id).all()
 
-    for city in results:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    # Query: unimos State y City, ordenamos por City.id
+    results = (
+        session.query(State.name, City.id, City.name)
+        .join(City)
+        .order_by(City.id.asc())
+        .all()
+    )
+
+    # Imprimir en el formato solicitado
+    for state_name, city_id, city_name in results:
+        print(f"{state_name}: ({city_id}) {city_name}")
