a68dff8395d9bee1ce0eb46f1061cd0e27a7b25f
geronimo304 <geronimonegreira06@gmail.com>
2025-09-09 11:32:54 -0300

    none

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,5 +3,7 @@ def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
         return my_list
-    else:
+    elif idx < 0:
+        return None
+    elif idx > len(my_list):
         return None
