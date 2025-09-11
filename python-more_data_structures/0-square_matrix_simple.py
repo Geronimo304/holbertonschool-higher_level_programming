5675783e16af573e1224e4be0cca1f3d9079523b
geronimo304 <geronimonegreira06@gmail.com>
2025-09-11 13:23:38 -0300

    matix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ptyhon3
+def square_matrix_simple(matrix=[]):
+    matrix_nueva = []
+    for i in matrix:
+        nueva = []
+        for j in i:
+            nueva.append(j ** 2)
+        matrix_nueva.append(nueva)
+    return matrix_nueva
