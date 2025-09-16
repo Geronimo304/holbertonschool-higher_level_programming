406656872c88f93d734f31821c5aea027e4d7ba3
geronimo304 <geronimonegreira06@gmail.com>
2025-09-16 17:47:41 -0300

    cuadrado

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""creo la clase."""
+
+
+class Square:
+    """clase que representa un cuadrado."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Decuelve el area del cuadrado"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """le asigna valor a size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """devuelve el area del cuadrado"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """imprime el cuadrado"""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
