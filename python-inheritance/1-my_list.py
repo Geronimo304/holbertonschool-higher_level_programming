59fad84586b72586dce7c21563686c4b2449f6f0
geronimo304 <geronimonegreira06@gmail.com>
2025-09-24 13:37:20 -0300

    test

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,10 +2,10 @@
 """ Declaramos la funcion. """
 
 
-
 class MyList(list): 
    """Creamos la class mylist heredada de list"""
    def print_sorted(self):
         """Declaramos la funcion"""
         lista = sorted(self)
         print(lista)
+        return lista
