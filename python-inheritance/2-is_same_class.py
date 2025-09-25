16f62b568baf4211084d9f64170bffd06d472b09
geronimo304 <geronimonegreira06@gmail.com>
2025-09-25 00:42:11 -0300

    documented

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def is_same_class(obj, a_class):
-    """Verifico que obj sea una instancia de la class"""
+    """ Verifico que obj sea una instancia de la class. """
     return type(obj) == a_class
