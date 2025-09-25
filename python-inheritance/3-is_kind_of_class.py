fb5257996764dbcebee9a95388bcd4f16bf3b356
geronimo304 <geronimonegreira06@gmail.com>
2025-09-25 09:55:57 -0300

    clase

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+""" Defino la funcion. """
+
+
 def is_kind_of_class(obj, a_class):
+    """ Uso return para saber si obj es un objeto de la clase"""
     return isinstance(obj, a_class)
