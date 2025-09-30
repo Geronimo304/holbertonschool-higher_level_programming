b8a5d3b939043e4ee7837d192df6b200cd51da10
geronimo304 <geronimonegreira06@gmail.com>
2025-09-30 09:57:14 -0300

    documentacion

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """Lee un archivo con estandar utf-8"""
     with open(filename, "r", encoding="utf-8") as archivo:
         archivo_completo = archivo.read()
         print(archivo_completo, end="")
