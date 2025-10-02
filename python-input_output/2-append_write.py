4f550c6aa2b80a70c0209d6e0208f5e2b469776b
geronimo304 <geronimonegreira06@gmail.com>
2025-10-02 10:42:29 -0300

    file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creo la funcion"""
+
+def append_write(filename="", text=""):
+    """Agrega una cadena al final de un archivo (UTF8)
+    y devuelve el número de caracteres agregados
+    """
+    with open(filename, "a", encoding="utf-8") as archivo:
+        return archivo.write(text)
