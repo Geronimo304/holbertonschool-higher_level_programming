04d5c6a90c62e89ca1e4c88feba26616a46a8bd8
geronimo304 <geronimonegreira06@gmail.com>
2025-10-14 17:17:52 -0300

    trabajo __dict__

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     def __init__(self, first_name, last_name, age):
-    """uso init para crear los objetos"""
+        """uso init para crear los objetos"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
