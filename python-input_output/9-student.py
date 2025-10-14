9932d191f279d299923af1f83eac0e5eac5324f2
geronimo304 <geronimonegreira06@gmail.com>
2025-10-14 17:13:46 -0300

    trabajo __dict__

---
+++ python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""inicializo la class"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+    """uso init para crear los objetos"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        """Devuelve los atributos como instancias"""
+        return alumno.__dict__
+
+alumno = Student("Geronimo", "Negreira", 18)
