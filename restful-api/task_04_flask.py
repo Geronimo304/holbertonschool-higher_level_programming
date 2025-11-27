247403bd0472483840b9ecf8a7dd263fa4967533
geronimo304 <geronimonegreira06@gmail.com>
2025-11-26 21:37:48 -0300

    test

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
 from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 users = {}
@@ -7,42 +7,36 @@ users = {}
 
 @app.route("/")
 def home():
-    return "Welcome to the Flask API!"
+    return f"Welcome to the Flask API!"
 
 
 @app.get("/data")
 def data():
-    """Return a list of all usernames."""
     return jsonify(list(users.keys()))
 
 
 @app.route("/status")
 def status():
-    """Return API status."""
-    return "OK"
+    return f"OK"
 
 
 @app.get("/users/<username>")
 def get_user(username):
-    """Return user data for a specific username."""
     if username not in users:
         return jsonify({"error": "User not found"}), 404
+
     return jsonify(users.get(username))
 
 
 @app.post("/add_user")
 def add_user():
-    """Add a new user if username is provided and not duplicated."""
     new_user = request.get_json()
 
     if not new_user or "username" not in new_user:
         return jsonify({"error": "Username is required"}), 400
 
-    username = new_user["username"]
-    if username in users:
-        return jsonify({"error": "Username already exists"}), 400
+    users[new_user["username"]] = new_user
 
-    users[username] = new_user
     return jsonify({"message": "User added", "user": new_user}), 201
 
 
